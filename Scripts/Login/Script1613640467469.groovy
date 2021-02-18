3012404075da12aaab635718eae268c4079a50e8
Gettha <Gettha@GETTHA>
2021-02-18 16:36:48 +0700

    Login

---
+++ Scripts/Login/Script1613640467469.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://beats-dev.beraucoal.co.id/')
+
+WebUI.setText(findTestObject('Object Repository/Page_HSE Automation/input_Login dengan SID Anda_v-textfield v-w_68bfb6'), 
+    'XIIKM')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_HSE Automation/input_Login dengan SID Anda_v-textfield v-w_68bfb6_1'), 
+    'Le5rtHwqZRU=')
+
+WebUI.click(findTestObject('Object Repository/Page_HSE Automation/div_Login'))
+
+WebUI.closeBrowser()
+
